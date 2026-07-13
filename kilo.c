dbd8b3ee5469cb74202d71b4f5e9274372a5102c
interaction <243127395@qq.com>
2026-07-13 22:28:44 +0800

    done ch04.82

--- kilo.c
+++ kilo.c
@@ -31,8 +31,10 @@ enum editorKey {
 /*** data ***/
 
 typedef struct erow {
-  int size;    // the text length
-  char *chars; // the actual text
+  int size;     // the text length
+  int rsize;    // <Tab> size
+  char *chars;  // the actual text
+  char *render; // chars to render as <Tab>
 } erow;
 
 struct editorConfig {
@@ -213,6 +215,20 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** row operations ***/
+
+void editorUpdateRow(erow *row) {
+  free(row->render);
+  row->render = malloc(row->size + 1);
+
+  int j;
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    row->render[idx++] = row->chars[j];
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len) {
   E.row = realloc(E.row, (E.numrows + 1) * sizeof(erow));
 
@@ -223,6 +239,12 @@ void editorAppendRow(char *s, size_t len) {
     die("malloc new char");
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+
+  editorUpdateRow(&E.row[at]);
+
   E.numrows += 1;
 }
 
@@ -323,12 +345,13 @@ void editorDrawRows(struct abuf *ab) {
         abAppend(ab, "~", 1);
       }
     } else {
-      int len = E.row[filerow].size - E.coloff;
+      int len = E.row[filerow].rsize - E.coloff;
+
       if (len < 0)
         len = 0;
       if (len > E.screencols)
         len = E.screencols;
-      abAppend(ab, &E.row[filerow].chars[E.coloff], len);
+      abAppend(ab, &E.row[filerow].render[E.coloff], len);
     }
 
     // clear from cursor position to end of line
